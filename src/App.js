8833eda6be10f0f83f77ae228caadbca9bf17fc5
Dylancouzon <dylancouzon@gmail.com>
2021-05-20 19:44:25 -0700

    Render data in form

--- src/App.js
+++ src/App.js
@@ -1,33 +1,80 @@
-import React from 'react';
+import React, { Component } from "react";
 
 import Jumbotron from './components/Jumbotron';
 import FilterInput from './components/FilterInput';
 import Container from 'react-bootstrap/Container';
 import BootstrapTable from 'react-bootstrap-table-next';
+import API from "./utils/API";
 
-const products = [{
-  id: 1,
-  name: "Test",
-  price: "$30"
-}];
-const columns = [{
-  dataField: 'id',
-  text: 'Product ID'
-}, {
-  dataField: 'name',
-  text: 'Product Name'
-}, {
-  dataField: 'price',
-  text: 'Product Price'
-}];
+const columns = [
+  {
+    dataField: 'picture',
+    text: 'Picture',
+    formatter: pictureFormatter
+  },
+  {
+    dataField: 'name',
+    text: 'Name'
+  }, {
+    dataField: 'age',
+    text: 'Age'
+  }, {
+    dataField: 'email',
+    text: 'Email'
+  },
+];
 
+function pictureFormatter(res) {
+    return <img src={res[0]} alt={res[1]} />;
+  }
+class App extends Component {
+  state = {
+    results: [],
+    filter: ""
+  };
 
-const App = () => (
-  <Container className="p-3">
-    <Jumbotron />
-    <FilterInput />
-    <BootstrapTable keyField='id' data={ products }  columns={ columns } bordered={ false }/>
-    </Container>
-);
+  componentDidMount() {
+    this.searchEmployee();
+  }
 
+  searchEmployee = () => {
+    API.search()
+      .then(res => {
+        const newData = [];
+        console.log(res.data);
+        res.data.results.forEach((data)=>{
+          newData.push({
+            picture: [data.picture.medium, data.name.first],
+            name: data.name.first + " " + data.name.last,
+            age: data.dob.age,
+            email: data.email
+          });
+        });
+        console.log(newData);
+        this.setState({ results: newData })
+      })
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const value = event.target.value;
+    this.setState({
+      filter: value
+    });
+  };
+
+  render() {
+    console.log(this.state.results);
+    return (
+      <Container className="p-3" >
+        <Jumbotron />
+        <FilterInput
+          value={this.state.filter}
+          handleInputChange={this.handleInputChange}
+        />
+        <BootstrapTable keyField='id' data={this.state.results} columns={columns} bordered={false} />
+      </Container>
+    );
+  }
+}
 export default App;
--- src/components/FilterInput.js
+++ src/components/FilterInput.js
@@ -4,13 +4,13 @@ import {InputGroup, FormControl, Col} from 'react-bootstrap/';
 
 
 
-const FilterInput = () => (
+const FilterInput = (props) => (
     <Col xs lg="5">
         <InputGroup className="mb-3" xs lg="2">
             <InputGroup.Prepend>
                 <InputGroup.Text>Employee Name:</InputGroup.Text>
             </InputGroup.Prepend>
-            <FormControl />
+            <FormControl onChange={props.handleInputChange} value={props.value} />
         </InputGroup>
     </Col>
 );
